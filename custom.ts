0ed3f1facb4c36bc958a46f52b54b0a918a4a421
LeviDog99 <83308663+LeviDog99@users.noreply.github.com>
2022-01-07 11:07:16 -0500

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,98 @@
+let Light = 0
+let Temperature = 0
+let Sound = 0
+let Compass = 0
+input.onButtonPressed(Button.A, function () {
+    Light = input.lightLevel()
+    Temperature = input.temperature()
+    Sound = input.soundLevel()
+})
+input.onButtonPressed(Button.AB, function () {
+    namespace Scanner {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+})
+basic.forever(function () {
+    Compass = input.compassHeading()
+    if (Compass < 45) {
+        basic.showString("N")
+    } else if (Compass < 135) {
+        basic.showString("E")
+    } else if (Compass < 225) {
+        basic.showString("S")
+    } else if (Compass < 315) {
+        basic.showString("W")
+    }
+})
+basic.forever(function () {
+	
+})
